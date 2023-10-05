5222d89e095bd5b9636360ba63ce70fb2409055b
Imad Mimouni <imimouni@e3r2p15.1337.ma>
2023-10-05 10:40:59 +0100

    update

--- irc_header.hpp
+++ irc_header.hpp
@@ -1,24 +1,26 @@
 #pragma once
 
-#include <iostream>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <netdb.h>
-#include <cstring>
-#include <cstdlib>
-#include <vector>
-#include <map>
-#include <poll.h>
-#include <unistd.h>
-#include <sstream>
-#include <string>
-#include <cerrno>
-#include <ctime>
+# include <iostream>
+# include <sys/types.h>
+# include <sys/socket.h>
+# include <netinet/in.h>
+# include <arpa/inet.h>
+# include <netdb.h>
+# include <cstring>
+# include <cstdlib>
+# include <vector>
+# include <map>
+# include <poll.h>
+# include <unistd.h>
+# include <sstream>
+# include <string>
+# include <cerrno>
+# include <ctime>
 
 class irc
 {
-private:
-public:
+    private:
+
+    public:
+
 };
