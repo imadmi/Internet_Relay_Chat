51ccf9f0fb88d595131530bd76928b4ad635958e
proxy <aittaleb108@gmail.com>
2023-10-08 09:55:35 +0100

    [refactor]

--- headers/exec.hpp
+++ headers/exec.hpp
@@ -6,7 +6,7 @@
 /*   By: otait-ta <otait-ta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/07 11:54:39 by otait-ta          #+#    #+#             */
-/*   Updated: 2023/10/07 13:08:20 by otait-ta         ###   ########.fr       */
+/*   Updated: 2023/10/08 09:48:23 by otait-ta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,11 @@
 
 #include "irc_header.hpp"
 
+/**
+ * @brief execute the command given by the client
+ * @param command the command to execute
+ * @param client the client who sent the command
+ * @param channels the channels of the server
+ * @return void
+ */
 void excute_command(std::string command, Client &client, std::map<std::string, Channel> &channels);
--- headers/join.hpp
+++ headers/join.hpp
@@ -6,7 +6,7 @@
 /*   By: otait-ta <otait-ta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/07 11:05:14 by otait-ta          #+#    #+#             */
-/*   Updated: 2023/10/07 13:50:00 by otait-ta         ###   ########.fr       */
+/*   Updated: 2023/10/08 09:49:52 by otait-ta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,13 @@
 
 #include "irc_header.hpp"
 #include "channel.hpp"
+
+/**
+ * @brief join a channel
+ * @param client the client who wants to join the channel
+ * @param channels the channels of the server
+ * @param channel_name the name of the channel to join
+ * @return int 0 if success, 1 if error
+ */
 
 int join(Client &client, std::map<std::string, Channel> &channels, std::string &channel_name);
