4de0a7b423d9e1af0df07bda2728da8eda6ecd1a
proxy <aittaleb108@gmail.com>
2023-10-12 18:08:06 +0100

    fix a seg in kick

--- src/commands/kick.cpp
+++ src/commands/kick.cpp
@@ -5,10 +5,13 @@
 void kick(std::string command, Client &client, std::map<std::string, Channel> &channels)
 {
     std::string channel_name = command.substr(5, command.find(" ", 5) - 5);
+    std::cout << "channel_name : " << channel_name << std::endl;
     std::string nickname = command.substr(command.find(" ", 5) + 1, command.length() - command.find(" ", 5) - 1);
+    std::cout << "nickname : " << nickname << std::endl;
     nickname = nickname.substr(0, nickname.find(" "));
-    std::string comment = command.substr(command.find(":", 5) + 1, command.length() - command.find(":", 5) - 1);
-
+    std::cout << "nickname : " << nickname << std::endl;
+    // std::string comment = command.substr(command.find(":", 5) + 1, command.length() - command.find(":", 5) - 1);
+    // std::cout << "comment : " << comment << std::endl;
     // print the first channel in the map
     if (channels.find(channel_name) == channels.end())
     {
@@ -28,8 +31,10 @@ void kick(std::string command, Client &client, std::map<std::string, Channel> &c
     }
     std::map<int, Client> &clients = channel.get_clients();
     std::map<int, Client>::iterator it = clients.begin();
+    std::cout << "channel_name : " << std::endl;
     while (it != clients.end())
     {
+        std::cout << "channel_name : " << std::endl;
         if (it->second.get_nickname() == nickname)
         {
             channel.remove_client(it->second);
@@ -40,5 +45,6 @@ void kick(std::string command, Client &client, std::map<std::string, Channel> &c
         }
         it++;
     }
+    std::cout << "im out " << std::endl;
     client.add_buffer_to_send(ERR_NOSUCHNICK(client.get_nickname(), channel_name));
 }
