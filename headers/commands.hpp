f0647867d0404d74600185eb58225ebdff3030d7
Enstientt <zikoessad@gmail.com>
2023-10-12 13:15:34 +0100

    nickname t7ayid

--- headers/commands.hpp
+++ headers/commands.hpp
@@ -3,30 +3,30 @@
 #include "Irc.hpp"
 #include "Channel.hpp"
 
-#define ERR_NONICKNAMEGIVEN(client) (": 431 " + client + " :There is no nickname.\r\n")
-#define ERR_ERRONEUSNICKNAME(client, nickname) (": 432 " + client + " " + nickname + " :Erroneus nickname\r\n")
+#define ERR_NONICKNAMEGIVEN(client) (":localhost 431 " + client + " :There is no nickname.\r\n")
+#define ERR_ERRONEUSNICKNAME(client, nickname) (":localhost 432 " + client + " " + nickname + " :Erroneus nickname\r\n")
 #define RPL_NICK(oclient, uclient, client) (": " + oclient + "!" + uclient + "@localhost NICK " + client + "\r\n")
-#define ERR_NICKNAMEINUSE(client, nickname) (": 433 " + client + " " + nickname + " :Nickname is already in use.\r\n")
-#define RPL_WELCOME(user_id, nickname) (": 001 " + nickname + " :Welcome to the Internet Relay Network " + user_id + "\r\n")
-#define RPL_YOURHOST(client, servername) (": 002 " + client + " :Your host is " + servername + "\r\n")
-#define RPL_CREATED(client, datetime) (": 003 " + client + " :This server was created " + datetime + "\r\n")
+#define ERR_NICKNAMEINUSE(client, nickname) (":localhost 433 " + client + " " + nickname + " :Nickname is already in use.\r\n")
+#define RPL_WELCOME(user_id, nickname) (":localhost 001 " + nickname + " :Welcome to the Internet Relay Network " + user_id + "\r\n")
+#define RPL_YOURHOST(client, servername) (":localhost 002 " + client + " :Your host is " + servername + "\r\n")
+#define RPL_CREATED(client, datetime) (":localhost 003 " + client + " :This server was created " + datetime + "\r\n")
 // channels
 #define RPL_JOIN(nickname, channel) (":" + nickname + " JOIN " + channel + "\r\n")
-#define ERR_USERONCHANNEL(client, nick, channel) (": 443 " + client + " " + nick + " " + channel + " is already on channel\r\n")
-#define ERR_NOSUCHCHANNEL(client, channel) (": 403 " + client + " " + channel + " :No such channel\r\n")
-#define ERR_NOTONCHANNEL(client, channel) (": 442 " + client + " " + channel + " :You're not on that channel.\r\n")
-#define ERR_CHANOPRIVSNEEDED(client, channel) (": 482 " + client + " " + channel + " :You're not channel operator.\r\n")
-#define RPL_KICK(client, channel) (": 441  #KICK " + channel + " " + client + " :You have been kicked from the channel.\r\n")
-#define ERR_NOSUCHNICK(client, nickname) (": 401 " + client + " " + nickname + " :No such nick/channel\r\n")
+#define ERR_USERONCHANNEL(client, nick, channel) (":localhost 443 " + client + " " + nick + " " + channel + " is already on channel\r\n")
+#define ERR_NOSUCHCHANNEL(client, channel) (":localhost 403 " + client + " " + channel + " :No such channel\r\n")
+#define ERR_NOTONCHANNEL(client, channel) (":localhost 442 " + client + " " + channel + " :You're not on that channel\r\n")
+#define ERR_CHANOPRIVSNEEDED(client, channel) (":localhost 482 " + client + " " + channel + " :You're not channel operator\r\n")
+#define RPL_KICK(client, channel) (":localhost 441 " + client + " " + channel + " :You have been kicked from the channel\r\n")
+#define ERR_NOSUCHNICK(client, channel) (":localhost 401 " + client + " " + channel + " :No such nick/channel\r\n")
 /// end channels
-#define ERR_PASSWDMISMATCH(client) (": 464 " + client + " :Password incorrect\r\n")
+#define ERR_PASSWDMISMATCH(client) (":localhost 464 " + client + " :Password incorrect\r\n")
 #define RPL_MYINFO(client, servername, version, user_modes, chan_modes, chan_param_modes) (":localhost 004 " + client + " " + servername + " " + version + " " + user_modes + " " + chan_modes + " " + chan_param_modes + "\r\n")
-#define ERR_NEEDMOREPARAMS(client, command) (": 461 " + client + " " + command + " :Not enough parameters.\r\n")
+#define ERR_NEEDMOREPARAMS(client, command) (":localhost 461 " + client + " " + command + " :Not enough parameters.\r\n")
 
-// privmsg
-#define ERR_NORECIPIENT(client) ("411 " + client + " :No recipient given PRIVMSG\r\n")
-#define ERR_NOTEXTTOSEND(client) ("412 " + client + " :No text to send\r\n")
-#define RPL_PRIVMSG(nick, username, target, message) (":" + nick + "!" + username + "@localhost PRIVMSG " + target + " " + message + "\r\n")
+//privmsg
+# define ERR_NORECIPIENT(client) ("411 " + client + " :No recipient given PRIVMSG\r\n")
+# define ERR_NOTEXTTOSEND(client) ("412 " + client + " :No text to send\r\n")
+# define RPL_PRIVMSG(nick, username, target, message) (":" + nick + "!" + username + "@localhost PRIVMSG " + target + " " + message + "\r\n")
 
 /**
  * @brief execute the command given by the client
@@ -36,7 +36,7 @@
  * @return void
  */
 void excute_command(std::string command, Client &client, std::map<std::string, Channel> &channels, std::map<int, Client> &clients);
-void privmsg(std::string message, Client &client, std::map<int, Client> &clients, std::map<std::string, Channel> &channels);
+void privmsg(std::string message, Client &client , std::map<int, Client> &clients, std::map<std::string, Channel> &channels);
 void broadcastTochannel(std::string message, std::string channel, std::map<std::string, Channel> &channels);
 /**
  * @brief join a channel
