50028306dfb5291b684724feba4cf226e4b466d0
imad <imad.mimouni.123@gmail.com>
2023-10-15 12:09:46 +0100

    update

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -8,6 +8,11 @@ Irc::Irc(int port, char *password)
     _port = port;
     if (_port < 1024 || _port > 65535)
         printc("The port is out of rang", RED, 1);
+    std::cout << ":::::     :::::    :::::::::    ::::   :::" << std::endl;
+    std::cout << "::::::   ::::::    :::          :::::  :::" << std::endl;
+    std::cout << "::: ::: ::: :::    :::::::::    ::: :: :::" << std::endl;
+    std::cout << ":::   ::    :::          :::    :::  :::::" << std::endl;
+    std::cout << ":::         :::    :::::::::    :::   ::::\n" << std::endl;
     _serverName = ":MSN ";
 
     createSocket();
@@ -151,54 +156,6 @@ void Irc::printc(std::string msg, std::string color, int ex)
         exit(EXIT_SUCCESS);
 }
 
-
-void*  dccFileTransfer(void* arg)
-{
-    std::string* file = static_cast<std::string*>(arg);
-
-    int send_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (send_fd < 0)
-    {
-        std::cout << "Error creating socket" << std::endl;
-        return NULL;
-    }
-    int opt = 1;
-    if (setsockopt(send_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)))
-    {
-        std::cout << "Error setting socket options" << std::endl;
-        return NULL;
-    }
-    struct sockaddr_in addr;
-    int addrlen = sizeof(addr);
-    memset(&addr, 0, addrlen);
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(9999);
-    addr.sin_addr.s_addr = INADDR_ANY;
-
-    if (bind(send_fd, (struct sockaddr *)&addr, sizeof(addr)) < 0)
-    {
-        std::cout << "Error binding bot socket" << std::endl;
-        return NULL;
-    }
-    if (listen(send_fd, 1) < 0)
-    {
-        std::cout << "Error listening on socket" << std::endl;
-        return NULL;
-    }
-    int new_socket = accept(send_fd, (struct sockaddr *)&addr, (socklen_t *)&addrlen);
-    if (new_socket < 0)
-    {
-        std::cout << "Error accepting connection" << std::endl;
-        return NULL;
-    }
-    send(new_socket, file->c_str(), file->size(), 0);
-    while(true);
-    // sleep(10);
-    close(new_socket);
-    close(send_fd);
-    return NULL;
-}
-
 void  Irc::handleBot(Client &client)
 {
 	std::string msg;
@@ -273,42 +230,6 @@ void Irc::recvClientsMsg(Client &client, std::string buffer)
 {
     client.addt_buffer(buffer);
 }
-
-// void Irc::handleBot(Client &client)
-// {
-//     std::string file_path = "/goinfre/imimouni/zoro.jpeg";
-
-//     char buffer[1000];
-//     FILE *fd = fopen(file_path.c_str(), "rb");
-//     std::string buff;
-//     while (!feof(fd))
-//     {
-//         int size = fread(&buffer, 1, 1000, fd);
-//         if (size < 0)
-//             break;
-//         buff.append(buffer, size);
-//     }
-
-
-
-//     std::string dccRequest = "DCC SEND";
-//     dccRequest += file_path;
-//     dccRequest += " 0 0";
-
-//     std::string bot = "user";
-
-
-//     // std::string privmsgCommand = "PRIVMSG " + client.get_nickname() + " :" + '\x01' + "DCC SEND " + "/goinfre/imimouni/zoro.jpeg" + " 0 6667 " + std::to_string(buff.size()) + '\x01';
-//     std::string privmsgCommand = "PRIVMSG " + bot  + " " + '\x01' + "DCC SEND " + "/goinfre/imimouni/zoro.jpeg" + " 10.12.11.2 6667 " + std::to_string(buff.size()) + '\x01';
-
-
-// // PRIVMSG user \x01 DCC SEND /goinfre/imimouni/zoro.jpeg 10.12.11.2 6667 420782\x01
-
-//     std::cout << privmsgCommand << std::endl;
-
-
-//     // send(client.get_fd(), privmsgCommand.c_str(), strlen(privmsgCommand.c_str()), 0);
-// }
 
 void Irc::handleQuotes(Client &client)
 {
